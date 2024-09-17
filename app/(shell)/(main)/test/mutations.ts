9f0c6e6524aea90e0fb4c827cff613a37058aeb9
Admin <support@unlimitpotential.com>
2024-09-17 02:36:05 -0400

    j

--- app/(shell)/(main)/test/mutations.ts
+++ app/(shell)/(main)/test/mutations.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const DELETE_SEGMENT = gql`
   mutation DeleteSegment($segmentId: String!) {
-    delete_Segment(where: { id2: { _eq: $segmentId } }) {
+    delete_Segment(where: { id: { _eq: $segmentId } }) {
       affected_rows
     }
   }
