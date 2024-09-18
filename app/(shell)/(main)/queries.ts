1c5f11abee00e55a2f09fad8746f73511e86cc8a
Admin <support@unlimitpotential.com>
2024-09-17 23:42:42 -0400

    h

--- app/(shell)/(main)/queries.ts
+++ app/(shell)/(main)/queries.ts
@@ -17,7 +17,7 @@ mutation PublishSegment($id: ID!, $post: [FormFieldInput]) {
 
 `;
 export const UPDATE_SEGMENT = gql`
-  mutation UpdateSegment($id: uuid!, $post: jsonb) {
+  mutation UpdateSegment($id: String!, $post: jsonb!) {
     update_Segment(
       where: { id: { _eq: $id } },
       _set: { post: $post }
