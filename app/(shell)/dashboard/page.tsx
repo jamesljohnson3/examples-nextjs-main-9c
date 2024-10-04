1d7d015eab2791f1a0c4c02fc6d8ad6cdeda313d
Admin <support@unlimitpotential.com>
2024-10-04 02:11:49 -0400

    j

--- app/(shell)/dashboard/page.tsx
+++ app/(shell)/dashboard/page.tsx
@@ -4,19 +4,13 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 import { Dashboard } from "@/components/Dashboard/Dashboard";
+import { AppSidebar } from "@/components/Sidebar/Sidebar";
 
-export default async function Page() {
-  const { cookies } = await import("next/headers")
+export default function Home() {
   return (
-    <SidebarLayout
-      defaultOpen={cookies().get("sidebar:state")?.value === "true"}
-    >
-      <MainSidebar />
-
+    <main className="grid gap-4 p-4 grid-cols-1 md:grid-cols-[220px,_1fr]">
+      <AppSidebar />
       <Dashboard />
-
-      
-      
-    </SidebarLayout>
-  )
+    </main>
+  );
 }
