8eaa539a860a42924225f92a91900c44992e474b
Admin <support@unlimitpotential.com>
2024-10-08 03:46:46 -0400

    j

--- env.mjs
+++ env.mjs
@@ -34,7 +34,7 @@ export const env = createEnv({
     
     
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    AUTH_SECRET: process.env.AUTH_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     
   },
   /**
